d4fec0563aee7ee5a4669bf8e4f769be69b1019d
JaeDragon <leejy0068@gmail.com>
2024-05-09 16:35:33 +0900

    패키지 변경

--- MainServer/NewMit/src/main/java/com/haezuo/newmit/ingredients/controller/ingredientsController.java
+++ MainServer/NewMit/src/main/java/com/haezuo/newmit/ingredients/controller/ingredientsController.java
@@ -1,4 +1,4 @@
-package com.haezuo.newmit.ingredients;
+package com.haezuo.newmit.ingredients.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
